1f74373294aa3622ccb075e98c14823ab7ea5254
Sander Kallio <sander.kallio@tuni.fi>
2020-03-05 22:07:44 +0200

    added hook for action

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const webhook = require("webhook-discord")
+ 
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  const user = core.getInput('user');
+  const url = core.getInput('url');
+  const name = core.getInput('name');
+  let message = core.getInput('message');
+
+  const index =  message.search('{name}');
+
+  if (index != -1) {
+      message = message.replace('{name}', '');
+      message = message.slice(0, index) + name + message.slice(index);
+  } else {
+      message += ", " + name;
+  }
+
+  const Hook = new webhook.Webhook(url)
+  const msg = new webhook.MessageBuilder()
+  .setName(user)
+  .setText(message);
+
+  Hook.send(msg);
+
+  console.log(message);
+
 } catch (error) {
   core.setFailed(error.message);
 }
